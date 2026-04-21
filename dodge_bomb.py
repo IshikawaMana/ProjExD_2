2dbcd59b5db104be5d9f5bf6995b19c38b064368
c0c250085b <c0c250085b@edu.teu.ac.jp>
2026-04-21 16:30:21 +0900

    時間とともに爆弾が拡大加速する/こうかとんの向きを変える

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,8 +4,8 @@ import pygame as pg
 import random
 import time
 
-
 WIDTH, HEIGHT = 1100, 650
+img03 = pg.image.load("ex2/fig/3.png")
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(rct):
@@ -43,11 +43,39 @@ def gameover(screen: pg.Surface) -> None:
 
     pg.time.wait(5000)
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = [] 
+    bb_accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        bb_img.set_colorkey((0, 0, 0))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+    return bb_imgs, bb_accs
+
+def get_kk_imgs() -> dict[tuple[int, int], pg.Surface]:
+    #左向き
+    img_left = pg.transform.rotozoom(img03, 0, 0.9)
+    #右向き
+    img_right = pg.transform.flip(img_left, True, False)
+
+    kk_imgs = {
+        (-5,  0): img_left,                                  # 左
+        (-5, -5): pg.transform.rotozoom(img_left, -45, 1.0), # 左上
+        ( 0, -5): pg.transform.rotozoom(img_right, 90, 1.0),   # 上
+        (+5, -5): pg.transform.rotozoom(img_right, 45, 1.0),   # 右上
+        (+5,  0): img_right,                                   # 右
+        (+5, +5): pg.transform.rotozoom(img_right, -45, 1.0),  # 右下
+        ( 0, +5): pg.transform.rotozoom(img_right, -90, 1.0),  # 下
+        (-5, +5): pg.transform.rotozoom(img_left, 45, 1.0),  # 左下
+    }
+    return kk_imgs
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_img = pg.transform.rotozoom(img03, 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_img = pg.Surface((20, 20))
@@ -55,18 +83,20 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+    
     vx = +5
     vy = +5
-
-    
-
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs, bb_accs = init_bb_imgs()
+    kk_imgs = get_kk_imgs()
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -83,6 +113,10 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += dx
                 sum_mv[1] += dy
+        
+        mv = tuple(sum_mv)
+        if mv in kk_imgs:
+            kk_img = kk_imgs[mv]
 
         kk_rct.move_ip(sum_mv)
 
@@ -95,7 +129,15 @@ def main():
         if tate == False:
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
-        bb_rct.move_ip(vx, vy)
+        bb_img = bb_imgs[min(tmr // 500, 9)]
+
+        old_center = bb_rct.center
+        bb_rct = bb_img.get_rect()
+        bb_rct.center = old_center
+
+        avx = vx * bb_accs[min(tmr // 500, 9)]
+        avy = vy * bb_accs[min(tmr // 500, 9)]
+        bb_rct.move_ip(avx, avy)
 
         bound = check_bound(bb_rct)
         bb_yoko = bound[0]
